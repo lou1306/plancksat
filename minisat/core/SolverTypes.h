lou1306/plancksat

commit 9d01b9e0bf3b544d10dbfc0f08de8c09832ea571

Better way of having single-argument mkLit()

minisat/core/SolverTypes.h
@@ -51,8 +51,6 @@ struct Lit {
     // "friend declaration specifying a default argument must be a definition"
     // friend Lit mkLit(Var var, bool sign = false);
     friend Lit mkLit(Var var, bool sign);
-    // This is for backwards compatibility w/ 3rd party tools
-    Lit mkLit(Var var);
     
 
     bool operator == (Lit p) const { return x == p.x; }
@@ -61,7 +59,7 @@ struct Lit {
 };
 
 
-inline  Lit  mkLit     (Var var, bool sign) { Lit p; p.x = var + var + (int)sign; return p; }
+inline  Lit  mkLit     (Var var, bool sign=false) { Lit p; p.x = var + var + (int)sign; return p; }
 inline  Lit  mkLit     (Var var)            { return mkLit(var, false); }
 inline  Lit  operator ~(Lit p)              { Lit q; q.x = p.x ^ 1; return q; }
 inline  Lit  operator ^(Lit p, bool b)      { Lit q; q.x = p.x ^ (unsigned int)b; return q; }

commit bac83e04d552ce551af3e0d895ba1890272568b5

Fix a bug from with mkLit
Introduced in 9d01b9e0bf3b544d10dbfc0f08de8c09832ea571

minisat/core/SolverTypes.h
@@ -60,7 +60,6 @@ struct Lit {
 
 
 inline  Lit  mkLit     (Var var, bool sign=false) { Lit p; p.x = var + var + (int)sign; return p; }
-inline  Lit  mkLit     (Var var)            { return mkLit(var, false); }
 inline  Lit  operator ~(Lit p)              { Lit q; q.x = p.x ^ 1; return q; }
 inline  Lit  operator ^(Lit p, bool b)      { Lit q; q.x = p.x ^ (unsigned int)b; return q; }
 inline  bool sign      (Lit p)              { return p.x & 1; }